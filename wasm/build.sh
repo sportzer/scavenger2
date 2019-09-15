fd04ef6e91331acd82ac57b41151c860d2fe645d
Steven Portzer <stevenportzer@gmail.com>
2019-09-14 20:34:03 -0700

    Move packages into a common workspace and update dependencies

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd -- "$(dirname -- "$0")" &&\
-    cargo build --release --target wasm32-unknown-unknown &&\
-    wasm-bindgen --no-typescript --no-modules target/wasm32-unknown-unknown/release/scavenger_wasm.wasm --out-dir . &&\
+    cargo build --release --package scavenger-wasm --target wasm32-unknown-unknown &&\
+    wasm-bindgen --no-typescript --no-modules ../target/wasm32-unknown-unknown/release/scavenger_wasm.wasm --out-dir . &&\
     zip scavenger.zip *.wasm *.js *.html rot.js/*
